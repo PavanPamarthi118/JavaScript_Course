2b42237bd73f2d3bf14e8bf378de540e62958615
PavanPamarthi118 <PavanPamarthi07@gmail.com>
2025-11-07 16:50:25 +0530

    Refactor cart management to persist data in localStorage and implement item removal in checkout

--- javascript-amazon-project/javascript-amazon-project/data/cart.js
+++ javascript-amazon-project/javascript-amazon-project/data/cart.js
@@ -1,11 +1,17 @@
-export const cart = [{
-    productId: 'e43638ce-6aa0-4b85-b27f-e1d07eb678c6',
-    quantity: 2
-},
-{
-    productId: '15b6fc6f-327a-4ec4-896f-486349e85a3d',
-    quantity: 1
-}];
+export let cart = localStorage.getItem('cart')
+    ? JSON.parse(localStorage.getItem('cart'))
+    : [{
+        productId: 'e43638ce-6aa0-4b85-b27f-e1d07eb678c6',
+        quantity: 2
+    },
+    {
+        productId: '15b6fc6f-327a-4ec4-896f-486349e85a3d',
+        quantity: 1
+    }];
+
+function saveToStorage() {
+    localStorage.setItem('cart', JSON.stringify(cart));
+}
 
 export function AddToCart(productId) {
     let matchingItem;
@@ -23,6 +29,14 @@ export function AddToCart(productId) {
             productId: productId,
             quantity: 1
         });
-
     }
+    saveToStorage();
+}
+
+export function RemoveFromCart(productId) {
+    const itemIndex = cart.findIndex(item => item.productId === productId);
+    if (itemIndex !== -1) {
+        cart.splice(itemIndex, 1);
+    }
+    saveToStorage();
 }
